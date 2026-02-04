9aa90af5e347f4753f846ce73b8250abc9fda3a1
MG <MG@MacBook-Air-de-Mehdi.local>
2026-02-04 21:53:03 +0100

    Ajout de l'API et 2 methode / et /howmany

--- api/main.py
+++ api/main.py
@@ -1,3 +1,35 @@
-from fastapi import FastApi
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+import random
 
-app = FastApi
+app = FastAPI()
+
+
+# Charge et retourne une liste de Map {quote - author} depuis le fichier quotes.txt
+def loadQuotes() -> list:
+    # fichier de citations
+    quotesFilename = "./quotes.txt"
+    quoteTab = []
+    with open(quotesFilename, "r", encoding="utf-8") as f:
+        for line in f:
+            quoteLine = line.split("-")
+            quote = {"quote": quoteLine[0].strip(), "author": quoteLine[1].strip()}
+            quoteTab.append(quote)
+    f.close()
+    return quoteTab
+
+
+QUOTES_LIST = loadQuotes()
+
+
+@app.get("/")
+async def get_random_quote():
+    # selectionner un index random dans quotesTab
+    content = random.choice(QUOTES_LIST)
+    return JSONResponse(content=content, media_type="application/json; charset=utf-8")
+
+
+@app.get("/howmany")
+async def get_total_quotes_count():
+    content = {"nbQuotes": len(QUOTES_LIST)}
+    return JSONResponse(content=content, media_type="application/json")
