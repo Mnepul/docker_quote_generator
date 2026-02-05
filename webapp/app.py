e9147f277cb31e8864ce56e2fdc8e63f752ab02b
MG <MG@MacBook-Air-de-Mehdi.local>
2026-02-05 17:41:08 +0100

    Template index.html + code WebApp Flask en dur

--- webapp/app.py
+++ webapp/app.py
@@ -1,4 +1,15 @@
 from flask import Flask, render_template
 import requests
 
-if __main__ == "__init__":
+app = Flask(__name__)
+
+
+@app.route("/")
+@app.route("/index")
+def index():
+    quote = "Une hirondelle ne fait pas le printemps"
+    author = "Anonyme"
+    return render_template("index.html", quote=quote, author=author)
+
+
+app.run(host="0.0.0.0", port=5000)
